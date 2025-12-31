eae0b28315f8570ac162eb5435eea1344d6b3360
Yogesh Tripathi <tripathi.yogesh31@gmail.com>
2025-12-31 20:01:42 +0530

    string rename from read more to more programs

--- src/components/sections/OfferingsSection.tsx
+++ src/components/sections/OfferingsSection.tsx
@@ -86,7 +86,7 @@ const OfferingsSection = () => {
               className="group border-primary/20 hover:bg-primary/5 text-primary"
               onClick={() => navigate('/offerings')}
             >
-              Read More
+              More Programs
               <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
             </Button>
           </div>
